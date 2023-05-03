c215c0bf52be12c3045c03e4b4a36ec804b09cdf
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-05-02 21:04:32 -0400

    Criando classe e atributos

---
+++ src/main/java/br/edu/ifms/aula/professor/Professor.java
@@ -0,0 +1,12 @@
+package br.edu.ifms.aula.professor;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Professor {
+	private Long id;
+	private String nome;
+	private String email;
+	
+	
+}
